53216209a1e278913d25e174b48ed923ff2acbcf
Daswart <dswart@roc-dev.com>
2021-06-18 10:28:30 +0200

    Revert "Update config.php"
    This reverts commit a4f68781b3cba4c851dc8ea206ff1bbe4a8e2120.

--- lib/backend/config.php
+++ lib/backend/config.php
@@ -2,7 +2,7 @@
 
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'paniek');
+define('DB_PASSWORD', '');
 define('DB_NAME', 'typeracer');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, 3308);
