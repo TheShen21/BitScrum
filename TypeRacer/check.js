3e80168af38298c5c104d633236df7d52872d23d
Catzu <that-@hotmail.nl>
2021-06-18 15:50:13 +0200

    checkCharachters
    Different character checker.
    uses for loop, length counter, if statement
    
    Ty Raul

---
+++ TypeRacer/check.js
@@ -0,0 +1,49 @@
+function compareChars(x, y) {
+    return x === y;
+    // if (x === y) {
+    //     //nothing
+    // }
+    // else (
+    //     alert("not good")
+    // )
+}
+
+function checkInput(input, code) {
+    //alert("there is input! cool");
+    var index = input.length - 1;
+
+    var charX = input[index];
+    var charY = code[index];
+
+    return compareChars(charX, charY);
+}
+
+function checkCharacters(input, code) {
+    var amount_right = 0;
+    var amount_wrong = 0;
+    for(var i = 0; i < input.length; i ++) {
+        if(input.charAt(i) == code.charAt(i)) {
+            //handle correct char
+            amount_right = amount_right+1
+            console.log("CHAR "+i+" IS CORRECT")
+            colorGreen(i)
+        }
+        else {
+            //handle wrong char
+            amount_wrong = amount_wrong+1
+            console.log("CHAR "+i+" IS WRONG")
+            colorRed(i)
+        }
+    }
+    return amount_right
+}
+
+function colorGreen(character) {
+    console.log("Coloring: " + character + " green")
+}
+
+function colorRed(character) {
+    console.log("Coloring: " + character + " red")
+}
+
+//checkCharacters("var x = 0", "var y != 3") string is working but other variables aren't
--- TypeRacer/game.js
+++ TypeRacer/game.js
@@ -1,6 +1,38 @@
+<<<<<<< Updated upstream
 function timer() {
     const startingMinutes = 1;
     let time = startingMinutes * 60;
+=======
+var characters = 0;
+var finalScore = 0;
+var msg = [ //this part needs to be connected to db 
+    "<p>My first paragraph.</p>",
+    "<h1>This is a heading</h1>",
+    "<button>Click me</button>"
+];
+
+var selected_msg = '';
+
+function randomText (){
+    var random = Math.floor(Math.random() * 3);
+    selected_msg = (msg[random]);
+    document.getElementById("textPreview").innerHTML = selected_msg;   
+}   
+
+function countChar(inputText) {
+    characters = inputText.value.length
+};
+
+var running = false;
+
+function startTimer() {
+    if (running == true) {
+        return
+    }
+    running = true;
+
+    time = 3;
+>>>>>>> Stashed changes
 
     const countdownTimer = document.getElementById('timer');
 
